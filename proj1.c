118aa28de250c2c3d38f43b625e8a5084a9ba4ee
antonioromeu <romeupinheiro27@gmail.com>
2019-03-21 16:34:07 +0000

    initial commit

---
+++ proj1.c
@@ -0,0 +1,13 @@
+// Antonio Romeu 92427
+
+#include  <stdio.h>
+
+typedef struct {
+    char descricao[63];
+    int dia;
+    int tempoInicio;
+    int duracao;
+    int sala;
+    char pessoaResponsavel[63];
+    char participantes;
+} Evento;
